6c77635f9d6880881543c1a1bd42fd09a3b5a9d5
Ritin Malhotra <rm5486@nyu.edu>
2023-08-18 02:20:31 +0530

    Update sync.js

--- sync.js
+++ sync.js
@@ -6,45 +6,46 @@ const path = require('path');
 const REPO_URL = 'https://raw.githubusercontent.com/CITIES-Dashboard/cities-dashboard.github.io/main/frontend/src/temp_database.json';
 
 async function fetchProjects() {
-  const response = await fetch(REPO_URL);
-  if (!response.ok) {
-    throw new Error('Failed to fetch projects JSON');
-  }
-  return await response.json();
+    const response = await fetch(REPO_URL);
+    if (!response.ok) {
+        throw new Error('Failed to fetch projects JSON');
+    }
+    return await response.json();
 }
 
 async function authenticate() {
-  const jwt = new google.auth.JWT(
-    process.env.GOOGLE_CLIENT_EMAIL,
-    null,
-    process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), 
-    ['https://www.googleapis.com/auth/spreadsheets']
-  );
-  await jwt.authorize();
-  return jwt;
+    const jwt = new google.auth.JWT(
+        process.env.GOOGLE_SHEETS_CREDS.client_email,
+        null,
+        process.env.GOOGLE_SHEETS_CREDS.private_key.replace(/\\n/g, '\n'),
+        ['https://www.googleapis.com/auth/spreadsheets']
+    );
+    await jwt.authorize();
+    return jwt;
 }
 
 async function fetchDataFromSheet(jwt, spreadsheetId, gid) {
-  const sheets = google.sheets({ version: 'v4', auth: jwt });
-  const { data } = await sheets.spreadsheets.values.get({
-    spreadsheetId: spreadsheetId,
-    range: `'${gid}'!A:Z`, 
-  });
-  return data.values;
+    const sheets = google.sheets({ version: 'v4', auth: jwt });
+    const { data } = await sheets.spreadsheets.values.get({
+        spreadsheetId: spreadsheetId,
+        range: `'${gid}'!A:Z`,
+    });
+    return data.values;
 }
 
 (async () => {
-  try {
-    const projects = await fetchProjects();
-    
-    const jwt = await authenticate();
+    try {
+        const projects = await fetchProjects();
 
-    for (let project of projects) {
-      // Continue with your logic to handle each project's data...
-      const data = await fetchDataFromSheet(jwt, project.sheetId, project.gid);
-      // Continue with your logic to save each dataset to your repo...
+        const jwt = await authenticate();
+
+        for (let project of projects) {
+            // Continue with your logic to handle each project's data...
+            const data = await fetchDataFromSheet(jwt, project.sheetId, project.gid);
+            // Continue with your logic to save each dataset to your repo...
+            const filename = path.join(__dirname, `../data/${project.slug}.json`);
+        }
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
     }
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-  }
 })();
