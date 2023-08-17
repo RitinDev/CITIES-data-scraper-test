772c190f7b5dd52fd56358761fdc3905b876f542
Ritin Malhotra <rm5486@nyu.edu>
2023-08-18 02:43:34 +0530

    Update Scripts

--- sync.js
+++ sync.js
@@ -1,52 +1,72 @@
-const { google } = require('googleapis');
-const fetch = require('node-fetch');
 const fs = require('fs');
-const path = require('path');
+const fetch = require('node-fetch');
+const { google } = require('googleapis');
 
-const REPO_URL = 'https://raw.githubusercontent.com/CITIES-Dashboard/cities-dashboard.github.io/main/frontend/src/temp_database.json';
+const SHEETS_API_KEY = 'AIzaSyCjVRS9swFZFN8FQq9ChM0FHWb_kRc0LCI'; // Ensure you set this as a secret in your GitHub repo
+const TEMP_DATABASE_URL = 'https://raw.githubusercontent.com/CITIES-Dashboard/cities-dashboard.github.io/main/frontend/src/temp_database.json';
 
-async function fetchProjects() {
-    const response = await fetch(REPO_URL);
+const fetchDataFromGithub = async () => {
+    const response = await fetch(TEMP_DATABASE_URL);
     if (!response.ok) {
-        throw new Error('Failed to fetch projects JSON');
+        throw new Error(`GitHub Data Fetch Error: ${response.statusText}`);
     }
     return await response.json();
-}
+};
 
-async function authenticate() {
-    console.log("GOOGLE_SHEETS_CREDS:", process.env.GOOGLE_SHEETS_CREDS)
-    const jwt = new google.auth.JWT(
-        process.env.GOOGLE_SHEETS_CREDS.client_email,
-        null,
-        process.env.GOOGLE_SHEETS_CREDS.private_key.replace(/\\n/g, '\n'),
-        ['https://www.googleapis.com/auth/spreadsheets']
-    );
-    await jwt.authorize();
-    return jwt;
-}
-
-async function fetchDataFromSheet(jwt, spreadsheetId, gid) {
-    const sheets = google.sheets({ version: 'v4', auth: jwt });
-    const { data } = await sheets.spreadsheets.values.get({
-        spreadsheetId: spreadsheetId,
-        range: `'${gid}'!A:Z`,
+const getSheetNameByGid = async (sheetId, gid) => {
+    const sheets = google.sheets({ version: 'v4' });
+    const response = await sheets.spreadsheets.get({
+        key: SHEETS_API_KEY,
+        spreadsheetId: sheetId,
+        fields: 'sheets(properties(sheetId,title))',
     });
-    return data.values;
-}
 
-(async () => {
-    try {
-        const projects = await fetchProjects();
+    const sheet = response.data.sheets.find(sheet => sheet.properties.sheetId === parseInt(gid));
+    return sheet ? sheet.properties.title : null;
+};
 
-        const jwt = await authenticate();
-
-        for (let project of projects) {
-            // Continue with your logic to handle each project's data...
-            const data = await fetchDataFromSheet(jwt, project.sheetId, project.gid);
-            // Continue with your logic to save each dataset to your repo...
-            const filename = path.join(__dirname, `../data/${project.slug}.json`);
-        }
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
+const fetchDataFromGoogleSheet = async (sheetId, gid) => {
+    const sheetName = await getSheetNameByGid(sheetId, gid);
+    if (!sheetName) {
+        throw new Error(`Sheet with GID ${gid} not found in spreadsheet ${sheetId}`);
     }
-})();
+
+    const sheets = google.sheets({ version: 'v4' });
+    const response = await sheets.spreadsheets.values.get({
+        key: SHEETS_API_KEY,
+        spreadsheetId: sheetId,
+        range: sheetName,
+    });
+    return response.data.values;
+};
+
+const arrayToCSV = (data) => {
+    return data.map(row => row.join(',')).join('\n');
+};
+
+const main = async () => {
+    const database = await fetchDataFromGithub();
+
+    for (const project of database) {
+        const projectPath = `./${project.id}`;
+        if (!fs.existsSync(projectPath)) {
+            fs.mkdirSync(projectPath);
+        }
+
+        for (const dataset of project.rawDataTables) {
+            const data = await fetchDataFromGoogleSheet(project.sheetId, dataset.gid);
+            const csv = arrayToCSV(data);
+
+            const filePath = `${projectPath}/${project.id}-${dataset.gid}.csv`;
+            if (!fs.existsSync(filePath) || fs.readFileSync(filePath, 'utf-8') !== csv) {
+                fs.writeFileSync(filePath, csv);
+            }
+        }
+
+        // Add metadata logic here
+        // Here, you would create/update metadata files (e.g., `datasets_metadata.json`)
+        // to track each CSV's raw link, date, size, etc.
+    }
+};
+
+main().catch(console.error);
