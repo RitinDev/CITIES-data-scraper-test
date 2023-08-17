be58fb7a473643c980d7dc7aee5ac5e08d6fdda6
Ritin Malhotra <rm5486@nyu.edu>
2023-08-18 02:10:31 +0530

    Update sync.js

--- sync.js
+++ sync.js
@@ -1,67 +1,50 @@
-const { google } = require('googleapis');
 const fs = require('fs');
-const path = require('path');
+const axios = require('axios');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 
-const sheets = google.sheets('v4');
+// Configuration constants
+const REPO_URL = 'https://raw.githubusercontent.com/CITIES-Dashboard/cities-dashboard.github.io/main/frontend/src/temp_database.json';
 
-const GOOGLE_SHEETS_CREDS = JSON.parse(process.env.GOOGLE_SHEETS_CREDS);
-
-async function authorize() {
-    const jwtClient = new google.auth.JWT(
-        GOOGLE_SHEETS_CREDS.client_email,
-        null,
-        GOOGLE_SHEETS_CREDS.private_key,
-        ['https://www.googleapis.com/auth/spreadsheets']
-    );
-    await jwtClient.authorize();
-    return jwtClient;
+async function fetchDatabase() {
+    try {
+        const response = await axios.get(REPO_URL);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching the temp_database.json:', error);
+        throw error;
+    }
 }
 
-async function getSheetNameByGID(sheetId, gid) {
-    const spreadsheetData = await sheets.spreadsheets.get({
-        auth: auth,
-        spreadsheetId: sheetId
-    });
+async function fetchDataFromSheet(sheetId, gid) {
+    // Initialize the sheets API client
+    const doc = new GoogleSpreadsheet(sheetId);
+    await doc.useServiceAccountAuth(JSON.parse(process.env.GOOGLE_SHEETS_CREDS));
+    await doc.loadInfo();
 
-    for (let sheet of spreadsheetData.data.sheets) {
-        if (sheet.properties.sheetId.toString() === gid.toString()) {
-            return sheet.properties.title;
+    // Assume that the sheet's title can be fetched using the gid.
+    const sheet = doc.sheetsById[gid];
+    const rows = await sheet.getRows();
+    return rows.map(row => row._rawData);
+}
+
+(async () => {
+    try {
+        const database = await fetchDatabase();
+
+        for (const project of database) {
+            if (project.rawDataTables) {
+                for (const dataset of project.rawDataTables) {
+                    const data = await fetchDataFromSheet(project.sheetId, dataset.gid);
+                    const csv = data.map(row => row.join(',')).join('\n');
+
+                    // Save the CSV to a file.
+                    // You can further enhance this to save in desired directories or filenames
+                    fs.writeFileSync(`./${project.id}-${dataset.gid}.csv`, csv);
+                }
+            }
         }
+
+    } catch (error) {
+        console.error('Error:', error);
     }
-    throw new Error(`No sheet found with gid ${gid}`);
-}
-
-async function fetchGoogleSheet(sheetId, gid) {
-    const sheetName = await getSheetNameByGID(sheetId, gid);
-
-    const sheetData = await sheets.spreadsheets.values.get({
-        auth: auth,
-        spreadsheetId: sheetId,
-        range: sheetName
-    });
-    return sheetData.data.values;
-}
-
-async function fetchAndSaveDataset(project) {
-    for (let dataset of project.rawDataTables) {
-        const data = await fetchGoogleSheet(project.sheetId, dataset.gid);
-        const csv = data.map(row => row.join(",")).join("\n");
-        fs.writeFileSync(path.join(project.id, `${dataset.gid}.csv`), csv);
-    }
-}
-
-async function main() {
-    const auth = await authorize();
-    const db = JSON.parse(fs.readFileSync('temp_database.json', 'utf8'));
-
-    for (let project of db) {
-        if (!fs.existsSync(project.id)) {
-            fs.mkdirSync(project.id);
-        }
-        await fetchAndSaveDataset(project);
-    }
-}
-
-main().catch(error => {
-    console.error('Error:', error);
-});
+})();
