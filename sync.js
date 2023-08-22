e2cd98502b3232d533ef7d6de861dad97f70ebc6
Ritin Malhotra <rm5486@nyu.edu>
2023-08-22 19:37:09 +0530

    Update Script to Format Size Attribute of Metadata

--- sync.js
+++ sync.js
@@ -81,9 +81,10 @@ const main = async (apiKey, databaseUrl) => {
     }
 
     for (const project of database) {
+        // Skip projects with no public datasets
         if (!project.rawDataTables ||
             project.rawDataTables.length === 0 ||
-            Object.keys(project.rawDataTables[0]).length === 0) continue;  // Skip projects with no public datasets
+            Object.keys(project.rawDataTables[0]).length === 0) continue;
 
         const projectPath = `./${project.id}`;
         if (!fs.existsSync(projectPath)) {
@@ -126,7 +127,7 @@ const main = async (apiKey, databaseUrl) => {
                 existingMetadata.lastModified = new Date().toISOString().split('T')[0];
             }
 
-            existingMetadata.size = size;
+            existingMetadata.size = size + " KB";
         }
 
         metadata[project.id] = projectMetadata;
