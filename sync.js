357762961b19ec18a611ecf0332e51f9d149d03f
Ritin Malhotra <rm5486@nyu.edu>
2023-08-26 20:16:47 +0530

    Revert Changes

--- sync.js
+++ sync.js
@@ -79,48 +79,60 @@ const main = async (apiKey, databaseUrl, currentCommit) => {
             project.rawDataTables.length === 0 ||
             Object.keys(project.rawDataTables[0]).length === 0) continue;
 
-        const projectMetadata = [];
+        const projectPath = `./${project.id}`;
+        if (!fs.existsSync(projectPath)) {
+            fs.mkdirSync(projectPath);
+        }
+
+        const projectMetadata = metadata[project.id] || {};
 
         for (const dataset of project.rawDataTables) {
             const { sheetName, data } = await fetchDataFromGoogleSheet(project.sheetId, dataset.gid, apiKey);
             const csvData = arrayToCSV(data);
 
+            let fileName;
             let sanitizedSheetName = "data"; // default
             if (sheetName) {
                 sanitizedSheetName = sheetName.toLowerCase().replace(/ /g, "-").replace(/[^a-z0-9-]/g, "_");
-            }
-            const fileName = `${project.id}-${sanitizedSheetName}.csv`;
-            const filePath = `./${project.id}/${fileName}`;
-
-            if (!fs.existsSync(filePath) || fs.readFileSync(filePath, 'utf-8') !== csvData) {
-                fs.writeFileSync(filePath, csvData, 'utf-8');
-            }
-
-            const sizeInKb = getCSVFileSize(filePath);
-
-            const versionInfo = {
-                name: sanitizedSheetName,
-                rawLink: `https://raw.githubusercontent.com/RitinDev/CITIES-data-scraper-test/${currentCommit}/${project.id}/${fileName}`,
-                version: new Date().toISOString().split('T')[0], // Using today's date as version
-                size: `${sizeInKb}`
-            };
-
-            if (metadata[project.id] && metadata[project.id][dataset.gid]) {
-                projectMetadata.push({
-                    id: dataset.gid,
-                    versions: [versionInfo, ...metadata[project.id][dataset.gid].versions]
-                });
+                fileName = `${project.id}-${sanitizedSheetName}.csv`;
             } else {
-                projectMetadata.push({
-                    id: dataset.gid,
-                    versions: [versionInfo]
-                });
+                fileName = `${project.id}-data.csv`;
+            }
+
+            const filePath = `${projectPath}/${fileName}`;
+            const oldCSVData = fs.existsSync(filePath) ? fs.readFileSync(filePath, 'utf-8') : "";
+            const oldHash = computeHash(oldCSVData);
+            const newHash = computeHash(csvData);
+
+            if (oldHash !== newHash) {
+                fs.writeFileSync(filePath, csvData);
+
+                const rawLinkLatest = `https://raw.githubusercontent.com/RitinDev/CITIES-data-scraper-test/main/${project.id}/${fileName}`;
+                const currentCommitRawLink = `https://raw.githubusercontent.com/RitinDev/CITIES-data-scraper-test/${currentCommit}/${project.id}/${fileName}`;
+                const size = getCSVFileSize(filePath);
+                const currentVersion = {
+                    name: sanitizedSheetName,
+                    rawLink: rawLinkLatest,
+                    version: new Date().toISOString().split('T')[0], // Only YYYY-MM-DD format
+                    sizeInKb: size
+                };
+
+                const datasetVersions = projectMetadata[dataset.gid] || [];
+
+                // If there is a previous version, update its rawLink to include the commit hash
+                if (datasetVersions.length > 0) {
+                    datasetVersions[0].rawLink = currentCommitRawLink;
+                }
+                datasetVersions.unshift(currentVersion);
+
+                projectMetadata[dataset.gid] = datasetVersions;
             }
         }
+
         metadata[project.id] = projectMetadata;
     }
 
-    fs.writeFileSync('./datasets_metadata.json', JSON.stringify(metadata, null, 2), 'utf-8');
+    fs.writeFileSync('./datasets_metadata.json', JSON.stringify(metadata, null, 2));
 };
 
 const SHEETS_API_KEY = process.env.SHEETS_NEW_API_KEY;
